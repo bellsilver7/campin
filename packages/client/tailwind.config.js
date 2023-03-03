120c3f95eafad5ac34f558282c754d30c2dbcac9
이종은 <bellsilver7@gmail.com>
2023-03-04 07:30:29 +0900

    client tailwindcss install

---
+++ packages/client/tailwind.config.js
@@ -0,0 +1,21 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+  mode: "jit",
+  purge: ["./src/**/*.{vue,js}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
